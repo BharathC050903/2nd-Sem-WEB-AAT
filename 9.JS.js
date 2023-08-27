5d72f96d4284af5b37e838ed202ff6209f16512e
BharathC <49397166+BharathC050903@users.noreply.github.com>
2023-08-27 17:51:46 +0530

    Add files via upload

---
+++ 9.JS.js
@@ -0,0 +1,17 @@
+var plan;
+function plana() {
+  plan = 199;
+}
+function planb() {
+  plan = 299;
+}
+function planc() {
+  plan = 399;
+}
+function tq() {
+  window.alert("Order Placed !");
+}
+document.getElementById("rname").innerHTML = "Bharath C";
+document.getElementById("rplan").innerHTML = "199";
+document.getElementById("rno").innerHTML = "9482544853";
+document.getElementById("raddress").innerHTML = "#112/113";
